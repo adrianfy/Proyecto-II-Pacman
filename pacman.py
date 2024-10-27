7606a6437e762e27be32e6ff7fc01ac69cc356ce
Adrian <adrianfernandez676@gmail.com>
2024-10-26 18:39:53 -0600

    Trabajando en el Laberinto

--- pacman.py
+++ pacman.py
@@ -4,20 +4,48 @@ from vector import Vector
 from constantes import *
 
 class Pacman(object):
-    def __init__(self):
+    def __init__(self, nodo):
         self.nombre = PACMAN
-        self.posicion = Vector(200, 400)
         self.direcciones = {DETENER:Vector(), ARRIBA:Vector(0,-1), ABAJO:Vector(0,1), IZQUIERDA:Vector(-1,0), DERECHA:Vector(1,0)}
         self.direccion = DETENER
         self.velocidad = 100 * ANCHOCASILLA/16
         self.radio = 10
         self.color = AMARILLO
+        self.nodo = nodo
+        self.setPosicion()
+        self.objetivo = nodo
+
+    def setPosicion(self):
+        self.posicion = self.nodo.posicion.copia()
 
     def actualizar(self, dt):
         self.posicion += self.direcciones[self.direccion]*self.velocidad*dt
         direccion = self.getTeclaValida()
-        self.direccion = direccion
+        if self.objetivoRebasado():
+            self.nodo = self.objetivo
+            self.objetivo = self.getNuevoObjetivo(direccion)
+            if self.objetivo is not self.nodo:
+                self.direccion = direccion
+            else:
+                self.objetivo = self.getNuevoObjetivo(self.direccion)
+            if self.objetivo is self.nodo:
+                self.direccion = DETENER
+            self.setPosicion()
+        else:
+            if self.direccionOpuesta(direccion):
+                self.direccionReversa()
 
+    def validarDirreccion(self, direccion):
+        if direccion is not DETENER:
+            if self.nodo.definirConexion[direccion] is not None:
+                return True
+        return False
+
+    def getNuevoObjetivo(self, direccion):
+        if self.validarDirreccion(direccion):
+            return self.nodo.definirConexion[direccion]
+        return self.nodo
+    
     # Movmimiento de Pacman segun las teclas
     def getTeclaValida(self):
         presionar_Tecla = pygame.key.get_pressed()
@@ -30,6 +58,27 @@ class Pacman(object):
         if presionar_Tecla[K_RIGHT] or presionar_Tecla[K_d]:
             return DERECHA
         return DETENER
+    
+    def objetivoRebasado(self):
+        if self.objetivo is not None:
+            vec1 = self.objetivo.posicion - self.nodo.posicion
+            vec2 = self.posicion - self.nodo.posicion
+            nodoAobjetivo = vec1.magnitudCuadrados()
+            nodoAself = vec2.magnitudCuadrados()
+            return nodoAself >= nodoAobjetivo
+        return False
+    
+    def direccionReversa(self):
+        self.direccion *= -1
+        temp = self.nodo
+        self.nodo = self.objetivo
+        self.objetivo = temp
+
+    def direccionOpuesta(self, direccion):
+        if direccion is not DETENER:
+            if direccion == self.direccion * -1:
+                return True
+        return False
     
     def renderizar(self, pantalla):
         p = self.posicion.coordenadaInt()
